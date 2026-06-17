eda28fc441e7aa26a034f4491acc1a1103747f63
Alan2113 <alan.ozga3@gmail.com>
2026-06-17 22:34:32 +0200

    LAB-04: prezentacja auto-deploy

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello DevOps 2026 — Alan (lab04 auto-deploy)"
+    return "Hello DevOps 2026 — Alan (test)"
 
 
 @app.route("/health")
